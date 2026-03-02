184307211d16f6cf2ee725ca8be65667c06ea2c8
s251388 <s251388@students.aie.edu.au>
2026-03-02 21:47:49 +1100

    Initial commit

---
+++ AIE-2.1.1-Print-Array/AIE-2.1.1-Print-Array.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+void PrintArray(int Array[], size_t Size) {
+
+	for (int i = 0; i < Size; i++) {
+		cout << Array[i] << endl;
+	}
+}
+
+
+int main()
+{
+    
+
+
+}
