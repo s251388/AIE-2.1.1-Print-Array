a9d44cef7dc7fe6a7d7d4b89dc22b55089aceb0f
s251388 <s251388@students.aie.edu.au>
2026-03-02 22:01:20 +1100

    2nd commit
    This commit adds the actual array initialization and calls the function.

--- AIE-2.1.1-Print-Array/AIE-2.1.1-Print-Array.cpp
+++ AIE-2.1.1-Print-Array/AIE-2.1.1-Print-Array.cpp
@@ -6,15 +6,19 @@ using namespace std;
 
 void PrintArray(int Array[], size_t Size) {
 
-	for (int i = 0; i < Size; i++) {
-		cout << Array[i] << endl;
-	}
+
+
+	for (int i = 0; i < Size; i++) {			// Here i have to pass the number of elements in the array through to the function separately as arrays behave
+		cout << Array[i] << endl;				// differently inside functions and cannot be passed in as a reference. Because every element in an array always
+	}											// uses the same amount of data, i can get this by dividing the total sizeof() by the sizeof() any one element.
+
 }
 
 
 int main()
 {
     
-
+	int Array[10] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+	PrintArray(Array, sizeof(Array) / sizeof(Array[0]));
 
 }
